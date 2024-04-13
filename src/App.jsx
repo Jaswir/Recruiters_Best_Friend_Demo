279939b9c737fa6152e03e08fb159bb5064b6d1d
Jaswir <jaswirraghoe@gmail.com>
2024-04-13 22:39:25 +0200

    React frontend uploading files works

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,11 @@ import axios from 'axios';
 function App() {
 
   const textInputRef = useRef(null);
+  const [company, setCompany] = useState('');
   const [response, setResponse] = useState('');
   const [loading, setLoading] = useState(false);
+
+  const [file, setFile] = useState(null);
 
   const handleButtonClick = async () => {
 
@@ -14,7 +17,8 @@ function App() {
 
     setLoading(true);
 
-    axios.get(`https://recruiters-best-friend-backend.vercel.app/query/?prompt=${queryInput}`)
+    axios.get(
+      `https://recruiters-best-friend-backend.vercel.app/query/?prompt=${queryInput}&company=${company}`)
       .then(response => {
         // Handle the response
         console.log(response.data);
@@ -29,16 +33,65 @@ function App() {
 
   };
 
+  const uploadFile = async () => {
+
+    const formData = new FormData();
+    formData.append('uploaded_file', file);
+
+    axios.post(`https://recruiters-best-friend-backend.vercel.app/upload/?company=${company}`,
+      formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+    })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  const handleCompanyChange = (event) => {
+    setCompany(event.target.value);
+  };
+
+  const handleFileChange = async (event) => {
+    setFile(event.target.files[0]);
+  };
+
   return (
 
     <div className="flex flex-col items-center justify-center w-full">
 
-      <input type="file" id="file-input" name="file-input" className="py-5" />
+
+
+
+      <input className='pt-5' type="file" id="file-input" name="file-input"
+        onChange={handleFileChange} />
+      <div className="pb-5">
+
+        <input
+          type="text"
+          placeholder="Enter Company Name"
+          className=" border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+          value={company}
+          onChange={handleCompanyChange}
+        />
+        <button
+          className="bg-blue-500 text-white px-4 py-2 rounded-md ml-2"
+          onClick={uploadFile}>
+          Upload
+        </button>
+      </div>
+
+
 
       <textarea
         className="flex w-1/2 rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 min-h-[150px] py-2"
         id="input"
         value={response}
+        readOnly={true}
       ></textarea>
 
       <div className="flex flex-row items-center py-5 w-1/2">
